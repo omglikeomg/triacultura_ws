7f4957f4ed84a814afde6de4d778fa75a3d927b9
demian <deliriux@gmail.com>
2018-02-16 16:41:37 +0100

    ara si

---
+++ TriaCultura_service/Controllers/TriaCulturaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TriaCultura_service.Models;
+
+namespace TriaCultura_service.Controllers
+{
+    public class TriaCulturaController : ApiController
+    {
+        // GET api/factures/{id}/{id2}
+        [Route("api/factures/{start_id?}/{end_id?}")]
+        public HttpResponseMessage Get(int start_id, int end_id)
+        {
+            var invoices = Repository.GetInvoices(start_id, end_id, true);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, invoices);
+            return response;
+        }
+
+        [Route("api/facturesTra/{track_id?}")]
+        public HttpResponseMessage Get_by_Track(int track_id)
+        {
+            var invoices = Repository.GetInvoicesTrack(track_id, true);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, invoices);
+            return response;
+        }
+
+        [Route("api/facturesCli/{customer_id?}")]
+        public HttpResponseMessage Get_by_Customer(int customer_id)
+        {
+            var invoices = Repository.GetInvoicesClient(customer_id, true);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, invoices);
+            return response;
+
+        }
+    }
--- TriaCultura_service/Models/Repository.cs
+++ TriaCultura_service/Models/Repository.cs
@@ -7,5 +7,43 @@ namespace TriaCultura_service.Models
 {
     public class Repository
     {
+        private static ChinookEntities dataContext = new ChinookEntities();
+        public static List<Invoice> GetInvoicesClient(int client_id, bool serialize)
+        {
+            List<Invoice> li = dataContext.Invoices.Where(x => x.CustomerId == client_id).ToList();
+            foreach (Invoice factura in li)
+            {
+                factura.SerializeVirtualProperties = serialize;
+            }
+            return li;
+        }
+
+        public static List<Invoice> GetInvoices(int start, int end, bool serialize)
+        {
+            List<Invoice> li = dataContext.Invoices.Where(x => x.InvoiceId >= start && x.InvoiceId <= end).ToList();
+            foreach (Invoice factura in li)
+            {
+                factura.SerializeVirtualProperties = serialize;
+            }
+            return li;
+        }
+
+        public static List<Invoice> GetInvoicesTrack(int track_id, bool serialize)
+        {
+            List<InvoiceLine> aux_list = dataContext.InvoiceLines.Where(x => x.TrackId == track_id).ToList();
+            List<Invoice> li = new List<Invoice>();
+            foreach (InvoiceLine line in aux_list)
+            {
+                if (!li.Contains(line.Invoice))
+                {
+                    li.Add(line.Invoice);
+                }
+            }
+            foreach (Invoice factura in li)
+            {
+                factura.SerializeVirtualProperties = serialize;
+            }
+            return li;
+
+        }
     }
-}
