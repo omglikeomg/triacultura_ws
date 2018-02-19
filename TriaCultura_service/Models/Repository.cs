f53985010fed4a65c9d0d27df0fdc13090daa91b
miquel <miquel@MIQUEL-PCCARLES>
2018-02-19 14:37:23 +0100

    repository acabat

--- TriaCultura_service/Models/Repository.cs
+++ TriaCultura_service/Models/Repository.cs
@@ -44,31 +44,77 @@ namespace TriaCultura_service.Models
         public static List<vote> GetVotes(int user_id, bool serialize)
         {
             List<vote> votes = context.votes.Where(x => x.user_id == user_id).OrderBy(x=> x.date).ToList();
-            foreach (vote v in votes)
+         /* foreach (vote v in votes)
             {
                 v.SerializeVirtualProperties = serialize;
-            }
+            }*/
                 return votes;
         }
 
         public static List<request> getAllRequest(bool serialize)
         {
             List<request> requests = context.requests.Where(x => x.is_winner == 1).OrderBy(x=>x.proposed_date).ToList();
-            foreach (request r in requests)
+           /* foreach (request r in requests)
             {
                 r.SerializeVirtualProperties = serialize;
-            }
+            }*/
             return requests;
         }
 
-        public static void putVote(int user_id, int project_id)
+        public static user putVote(int user_id, int project_id)
         {
             user u = context.users.Where(x => x.id == user_id).SingleOrDefault();
             project p = context.projects.Where(x=> x.id_project == project_id).SingleOrDefault();
             vote v = new vote();
+            v.project_id = p.id_project;
+            v.user_id = u.id;
+            v.date = DateTime.Now;
+            context.votes.Add(v);
+            context.SaveChanges();
+            return u;
+        }
+
+        public static void removeVote(int user_id, int project_id)
+        {
+            vote v = context.votes.Where(x=> x.user_id == user_id && x.project_id == project_id).SingleOrDefault();
+            context.votes.Remove(v);
+            context.SaveChanges();
 
         }
 
+        public static List<file> getfiles(int project_id, bool serialize)
+        {
+            List<file> files = context.files.Where(x => x.project_id == project_id).ToList();
+            /*foreach (file f in files)
+            {
+                f.SerializeVirtualProperties = serialize;
+            }*/
+            return files;
+        }
+
+        public static author getAuthorWhereProject(int project_id)
+        {
+            project p = context.projects.Where(x=> x.id_project == project_id).SingleOrDefault();
+            author a = p.author;
+            return a;
+        }
+        
+        public static int getCountRating(int project_id)
+        {
+            List<rating> ratings = context.ratings.Where(x => x.project_id == project_id).ToList();
+            
+            if (ratings.Count > 0)
+            {
+                int total = (int)ratings.Select(x => x.rate).Sum();
+                return total / ratings.Count;
+            }
+            return 0;
+        }
+        public static List<rating> getRatingsWhereAuthor(int user_id)
+        {
+            List<rating> ratings = context.ratings.Where(x => x.user_id == user_id).ToList();
+            return ratings;
+        }
 
 
     }
